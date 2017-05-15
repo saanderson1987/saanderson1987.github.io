048efe8facab8ee18aab670f21984a3339541802
Steven <saanderson1987@gmail.com>
2017-05-14 23:50:16 -0400

    site re-do

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,89 +1,43 @@
-/*
-	Telephasic by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
-(function($) {
+$(document).ready( () => {
+  // debugger;
+  $('.main-menu-button').click( () => {
+    $('.main-menu-items').toggle();
+    // $('.bars-container').toggleClass("menu-item-shown");
+  });
 
-	skel.breakpoints({
-		normal: '(max-width: 1280px)',
-		narrow: '(max-width: 1080px)',
-		narrower: '(max-width: 820px)',
-		mobile: '(max-width: 736px)',
-		mobilep: '(max-width: 480px)'
-	});
+  $('.skills-li').click( () => {
+    $('.skills-menu').toggle();
+    $('.skills-li').toggleClass("menu-item-shown");
+  });
 
-	$(function() {
+  $('.projects-li').click( () => {
+    $('.projects-menu').toggle();
+    $('.projects-li').toggleClass("menu-item-shown");
+  });
 
-		var	$window = $(window),
-			$body = $('body');
+  $(document).click ( (e) => {
+    if (
+      !($('.projects-menu').is(e.target)) &&
+      ($('.projects-menu').has(e.target).length === 0)
+      && !($('.projects-li').is(e.target))
+      && ($('.projects-li').has(e.target).length === 0)
+    ) {
+      $('.projects-menu').hide();
+      $('.projects-li').removeClass("menu-item-shown");
+    }
+  });
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+  $(document).click ( (e) => {
+    if (
+      !($('.skills-menu').is(e.target)) &&
+      ($('.skills-menu').has(e.target).length === 0)
+      && !($('.skills-li').is(e.target))
+      && ($('.skills-li').has(e.target).length === 0)
+    ) {
+      $('.skills-menu').hide();
+      $('.skills-li').removeClass("menu-item-shown");
+    }
+  });
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
-
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
-
-		// Prioritize "important" elements on narrower.
-			skel.on('+narrower -narrower', function() {
-				$.prioritize(
-					'.important\\28 narrower\\29',
-					skel.breakpoint('narrower').active
-				);
-			});
-
-		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
-
-		// Dropdowns.
-			$('#nav > ul').dropotron({
-				mode: 'fade',
-				speed: 300,
-				alignment: 'center',
-				noOpenerFade: true
-			});
-
-		// Off-Canvas Navigation.
-
-			// Navigation Button.
-				$(
-					'<div id="navButton">' +
-						'<a href="#navPanel" class="toggle"></a>' +
-					'</div>'
-				)
-					.appendTo($body);
-
-			// Navigation Panel.
-				$(
-					'<div id="navPanel">' +
-						'<nav>' +
-							'<a href="index.html" class="link depth-0">Home</a>' +
-							$('#nav').navList() +
-						'</nav>' +
-					'</div>'
-				)
-					.appendTo($body)
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'top',
-						target: $body,
-						visibleClass: 'navPanel-visible'
-					});
-
-			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#navButton, #navPanel, #page-wrapper')
-						.css('transition', 'none');
-
-	});
-
-})(jQuery);
+});
