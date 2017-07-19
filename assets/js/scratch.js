e60ef1b6dbc26f06a582e038f2de8203d665734c
Steven <saanderson1987@gmail.com>
2017-07-19 17:39:48 -0400

    create sass file

--- assets/js/scratch.js
+++ assets/js/scratch.js
@@ -1,15 +1,28 @@
+const toggleonClickPlusHighlight = (el1, el2) => {
+  el1.click ( () => {
+    el2.toggle();
+    el1.toggleClass("menu-item-shown");
+  });
+};
+
+const mainMenuItems = $('.main-menu-items').children();
+
+mainMenuItems.each ( (item) => {
+  let itemMenuClass = item.attr('class').replace('li', 'menu');
+  let itemMenu = $(itemMenuClass);
+  toggleonClickPlusHighlight(item, itemMenu);
+});
+
+
+
+
+
+
 
 
 const toggleOnClick = (el1, el2, el3) => {
   el1.click( () => {
     el2.toggle();
     el3.toggle();
-  });
-};
-
-const toggleonClickPlusHighlight = (el1, el2) => {
-  el1.click ( () => {
-    el2.toggle();
-    el1.toggleClass("menu-item-shown");
   });
 };
